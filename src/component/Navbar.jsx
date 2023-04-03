b4200cd92e66b603bd54b9193cb2cfefc49f7800
wirawan-mahardika <wirawanmahardika10@gmail.com>
2023-04-03 17:36:23 +0800

    menyelesaikan navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,8 +3,8 @@ import { Form } from "react-router-dom";
 export default function Navbar() {
   return (
     <>
-      <nav className='grid grid-cols-2 p-3 bg-emerald-500 gap-x-2'>
-        <Form className='px-4 py-1 bg-slate-200 rounded flex border border-black items-center'>
+      <nav className='grid grid-cols-2 fixed top-0 left-0 right-0 p-3 bg-emerald-500 gap-y-2'>
+        <Form className='px-2 py-1 bg-slate-200 rounded flex border border-black gap-x-2 items-center justify-between'>
           <svg
             xmlns='http://www.w3.org/2000/svg'
             fill='none'
@@ -19,9 +19,13 @@ export default function Navbar() {
             />
           </svg>
 
-          <input type='text' className='bg-inherit w-full' />
+          <input
+            type='text'
+            placeholder='Cari di Tokosedia'
+            className='bg-inherit w-full px-2 py-0.5 outline-none'
+          />
         </Form>
-        <div className='flex gap-x-2 justify-around p-2'>
+        <div className='flex gap-x-2 justify-around p-2 pr-0'>
           <svg
             xmlns='http://www.w3.org/2000/svg'
             fill='none'
@@ -76,7 +80,112 @@ export default function Navbar() {
             />
           </svg>
         </div>
+        <div className='flex'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='white'
+            className='w-6 h-6'>
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M15 10.5a3 3 0 11-6 0 3 3 0 016 0z'
+            />
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z'
+            />
+          </svg>
+          <p className='text-white'>
+            Dikirim ke <span className='font-bold'>Makassar</span>
+          </p>
+        </div>
       </nav>
+      <menu className='fixed grid grid-cols-6 grid-rows-1 text-xs gap-x-3 p-2 bottom-0 right-0 left-0 bg-stone-500'>
+        <div className='flex flex-col items-center'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke-width='1.5'
+            stroke='currentColor'
+            class='w-6 h-6'>
+            <path
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              d='M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25'
+            />
+          </svg>
+          <p>Home</p>
+        </div>
+        <div className='flex flex-col items-center'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke-width='1.5'
+            stroke='currentColor'
+            class='w-6 h-6'>
+            <path
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              d='M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z'
+            />
+          </svg>
+          <p>Feed</p>
+        </div>
+        <div className='flex flex-col items-center col-span-2'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke-width='1.5'
+            stroke='currentColor'
+            class='w-6 h-6'>
+            <path
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              d='M4.5 12.75l6 6 9-13.5'
+            />
+          </svg>
+          <p>Official Store</p>
+        </div>
+        <div className='flex flex-col items-center'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke-width='1.5'
+            stroke='currentColor'
+            class='w-6 h-6'>
+            <path
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              d='M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z'
+            />
+          </svg>
+          <p>Wishlist</p>
+        </div>
+        <div className='flex flex-col items-center'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke-width='1.5'
+            stroke='currentColor'
+            class='w-6 h-6'>
+            <path
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              d='M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z'
+            />
+          </svg>
+          <p>Transaksi</p>
+        </div>
+      </menu>
     </>
   );
 }
